5577be86c90acff5e41c44cf438a739f65a87d6c
Garvoz <afasavournin@gmail.com>
2024-12-20 17:33:22 +0100

    vendredi

--- application.py
+++ application.py
@@ -1,1 +1,11 @@
-import streamilt as st
+import streamlit as st
+
+st.title("Bienvenue dans le 5e Mur!")
+st.title("Bienvenue dans le Cinéscope!")
+st.title("Bienvenue CinéReco!")
+st.title("Bienvenue sur FilmWizard!")
+
+
+
+with st.sidebar:
+    st.title("Menu")
